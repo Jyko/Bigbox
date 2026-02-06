35b3ba6a853e98d92d413cd43451208a9ed8dc11
FERREIRA DA COSTA Julien <26435269+Jyko@users.noreply.github.com>
2026-02-06 16:16:27 +0100

    Add alias for kubectl watch command

--- modules/k8s/dotfiles/k8s_alias.sh
+++ modules/k8s/dotfiles/k8s_alias.sh
@@ -9,6 +9,7 @@ alias hu='h upgrade --install'
 # Aliases pour `Kubecolor`
 alias kubectl='kubecolor'
 alias k='kubecolor'
+alias kw='watch -n1 -c kubecolor --force-colors get'
 
 # Aliases pour `kubectx` et `kubens`
 alias kc='kubectx'
